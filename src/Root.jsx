1d952467ff37b705ffdd458f89c071ff1fe0db83
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-10 18:21:43 +0600

    build: appbar & heading

--- src/Root.jsx
+++ src/Root.jsx
@@ -12,9 +12,47 @@ import {
   Container,
   Button,
 } from "@mui/material";
+import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
 
 const Root = () => {
-  return <></>;
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCameraIcon />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+
+      <main>
+        <div>
+          <Container maxWidth="xs">
+            <Typography
+              variant="h3"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+
+            <Typography
+              variant="subtitle1"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+              laudantium asperiores porro. Ullam ipsum sapiente, quas saepe, et
+              facilis quam nobis aperiam in placeat, doloremque totam dolore
+              voluptas nesciunt voluptatibus!
+            </Typography>
+          </Container>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Root;
