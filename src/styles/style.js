1d71cabc88c4b3b52733b5c7ab122230a53b5377
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-11 12:05:05 +0600

    style: all card & footer

--- src/styles/style.js
+++ src/styles/style.js
@@ -6,6 +6,28 @@ const useStyles = makeStyles((theme) => ({
     padding: "20px 0",
     backgroundColor: "#f8f8f8",
   },
+  icon: {
+    marginRIght: "20px",
+  },
+  buttons: {
+    marginTop: "40px",
+  },
+  cardGrid: {
+    padding: "20px 0",
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", //16/9
+  },
+  cardConent: { flexGrow: "1" },
+  footer: {
+    background: "#f8f8f8",
+    padding: "50px 0",
+  },
 }));
 
 export default useStyles;
