33597f16bdd34bd2381e5ababe1cb408a44bd064
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-11 11:46:09 +0600

    style: cotainer

---
+++ src/styles/style.js
@@ -0,0 +1,11 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    // backgroundColor: theme.palette.background.paper, // For this need to learn theme
+    padding: "20px 0",
+    backgroundColor: "#f8f8f8",
+  },
+}));
+
+export default useStyles;
